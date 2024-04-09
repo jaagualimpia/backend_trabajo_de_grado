ade5b7cab8eb034b0de5de4fe0520e413c481dea
jaagualimpia <jaagualimpia25@gmail.com>
2024-04-08 22:23:43 -0500

    Little changes

--- backend_trabajo_de_grado/urls.py
+++ backend_trabajo_de_grado/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1.0/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
